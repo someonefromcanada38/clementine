b27c437e04c5342a97e82444680f26342be64b13
Andrew Irwin <someonefromcanada38@gmail.com>
2016-05-20 17:30:49 +0000

    Timestamp Microservice using Express

--- server.js
+++ server.js
@@ -1,22 +1,10 @@
 'use strict';
 
 var express = require('express');
-var routes = require('./app/routes/index.js');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var session = require('express-session');
 var moment = require('moment');
 
 var app = express();
-require('dotenv').load();
-require('./app/config/passport')(passport);
-
-mongoose.connect(process.env.MONGO_URI);
-
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
-app.get('/timeservice/:time', function(req,res)
+app.get('/:time', function(req,res)
 {
 	var requestdata = req.params.time;
 	var unixdate = null;
@@ -52,17 +40,6 @@ function unixToNormal(date) {
 	console.log('normal: '+moment.unix(date).format("MMMM D, YYYY"));
     return moment.unix(date).format("MMMM D, YYYY");
 }
-
-app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-routes(app, passport);
 
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
