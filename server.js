84f0f88c72dca06c6a44fca1335c1b7ffda911fe
Andrew Irwin <someonefromcanada38@gmail.com>
2016-05-20 16:55:48 +0000

    asdx

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var routes = require('./app/routes/index.js');
 var mongoose = require('mongoose');
 var passport = require('passport');
 var session = require('express-session');
+var moment = require('moment');
 
 var app = express();
 require('dotenv').load();
@@ -15,6 +16,42 @@ mongoose.connect(process.env.MONGO_URI);
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use('/common', express.static(process.cwd() + '/app/common'));
+app.get('/timeservice/:time', function(req,res)
+{
+	var requestdata = req.params.time;
+	var unixdate = null;
+	var normaldate = null;
+	
+	if (requestdata >= 0) 
+	{
+		console.log('Unix Date Detected');
+		console.log('unix: ' + requestdata);
+	    unixdate = requestdata;
+	    normaldate = unixToNormal(requestdata)
+	}
+	
+    if (isNaN(requestdata) && moment(requestdata, "MMMM D, YYYY").isValid()) 
+    {
+    	console.log('Normal Date Detected')
+    	console.log('normal: ' + requestdata);
+    	normaldate = requestdata;
+	    unixdate = normalToUnix(requestdata);
+	    
+    }
+    
+    var output = { "unix": unixdate, "date": normaldate };
+    res.send(JSON.stringify(output));
+});
+
+function normalToUnix(date) {
+	console.log('unix: '+moment(date, "MMMM D, YYYY").format("X"));
+    return moment(date, "MMMM D, YYYY").format("X");
+}
+
+function unixToNormal(date) {
+	console.log('normal: '+moment.unix(date).format("MMMM D, YYYY"));
+    return moment.unix(date).format("MMMM D, YYYY");
+}
 
 app.use(session({
 	secret: 'secretClementine',
