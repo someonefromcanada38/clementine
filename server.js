0c65ca42add3ff141179d1fc6258570076a41bdc
Andrew Irwin <someonefromcanada38@gmail.com>
2016-05-20 18:26:57 +0000

    new json and server for whoami

--- server.js
+++ server.js
@@ -4,42 +4,18 @@ var express = require('express');
 var moment = require('moment');
 
 var app = express();
-app.get('/:time', function(req,res)
+app.get('/whoami', function(req,res)
 {
-	var requestdata = req.params.time;
-	var unixdate = null;
-	var normaldate = null;
-	
-	if (requestdata >= 0) 
-	{
-		console.log('Unix Date Detected');
-		console.log('unix: ' + requestdata);
-	    unixdate = requestdata;
-	    normaldate = unixToNormal(requestdata)
-	}
-	
-    if (isNaN(requestdata) && moment(requestdata, "MMMM D, YYYY").isValid()) 
-    {
-    	console.log('Normal Date Detected')
-    	console.log('normal: ' + requestdata);
-    	normaldate = requestdata;
-	    unixdate = normalToUnix(requestdata);
-	    
-    }
-    
-    var output = { "unix": unixdate, "date": normaldate };
+	var requestdata = req.headers;
+	console.log(requestdata)
+	var output = {
+		'ipaddress': req.headers['x-forwarded-for'],
+		'language': req.headers['accept-language'].split(',')[0],
+		'software': req.headers['user-agent'].split('(')[1].split(')')[0]
+	};
     res.send(JSON.stringify(output));
 });
 
-function normalToUnix(date) {
-	console.log('unix: '+moment(date, "MMMM D, YYYY").format("X"));
-    return moment(date, "MMMM D, YYYY").format("X");
-}
-
-function unixToNormal(date) {
-	console.log('normal: '+moment.unix(date).format("MMMM D, YYYY"));
-    return moment.unix(date).format("MMMM D, YYYY");
-}
 
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
